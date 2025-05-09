b5f9ee6dc8f48a68b1edc30d1b397fca5e1a268c
meherett <meherett.batu@gmail.com>
2025-05-09 21:57:50 +0300

    Fix: SLIP10-Ed25519-Monero public key lengths

--- src/ecc/slip10/ed25519/monero/public-key.ts
+++ src/ecc/slip10/ed25519/monero/public-key.ts
@@ -5,6 +5,7 @@ import * as elliptic from 'elliptic';
 import { Point } from '../../../point';
 import { SLIP10Ed25519MoneroPoint } from './point';
 import { SLIP10Ed25519PublicKey } from '../../ed25519';
+import { SLIP10_ED25519_CONST } from '../../../../const';
 
 const ec = new elliptic.eddsa('ed25519');
 
@@ -16,6 +17,14 @@ export class SLIP10Ed25519MoneroPublicKey extends SLIP10Ed25519PublicKey {
 
   rawCompressed(): Uint8Array {
     return new Uint8Array(ec.encodePoint(this.publicKey));
+  }
+
+  static getCompressedLength(): number {
+    return SLIP10_ED25519_CONST.PUBLIC_KEY_BYTE_LENGTH;
+  }
+
+  static getUncompressedLength(): number {
+    return SLIP10Ed25519MoneroPublicKey.getCompressedLength();
   }
 
   point(): Point {
