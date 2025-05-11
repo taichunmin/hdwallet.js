8c22476be3f1fab0e705f2489575a495170483b4
meherett <meherett.batu@gmail.com>
2025-05-11 18:28:53 +0300

    Add: IndexType, DerivationType & DerivationsType types

---
+++ src/types.ts
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+export type IndexType = string | number | [number, number];
+
+export type DerivationType = [ number, boolean ];
+
+export type DerivationsType = DerivationType | [ number, number, boolean ];
