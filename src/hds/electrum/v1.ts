4965898168e4d9ac5925ef99ee652b946ce93bcf
meherett <meherett.batu@gmail.com>
2025-05-16 10:01:20 +0300

    Add: Electrum V1 & V2 HD implementations

---
+++ src/hds/electrum/v1.ts
@@ -0,0 +1,211 @@
+// SPDX-License-Identifier: MIT
+
+import { HD } from '../hd';
+import { ElectrumDerivation } from '../../derivations';
+import { Bitcoin } from '../../cryptocurrencies';
+import {
+  PublicKey, PrivateKey, SLIP10Secp256k1ECC, SLIP10Secp256k1PrivateKey, SLIP10Secp256k1PublicKey
+} from '../../ecc';
+import {
+  getBytes, bytesToString, bytesToInteger, integerToBytes, checkTypeMatch, concatBytes
+} from '../../utils';
+import { doubleSha256 } from '../../crypto';
+import { WIF_TYPES, PUBLIC_KEY_TYPES } from '../../const';
+import { privateKeyToWIF, wifToPrivateKey } from '../../wif';
+import { P2PKHAddress } from '../../addresses';
+import { SeedError, DerivationError, PrivateKeyError, PublicKeyError, WIFError, BaseError } from '../../exceptions';
+import { Seed } from '../../seeds';
+import { HDAddressOptionsInterface, HDOptionsInterface } from '../../interfaces';
+import { Buffer } from 'buffer';
+
+export class ElectrumV1HD extends HD {
+
+  protected seed?: Uint8Array;
+  protected masterPrivateKey?: PrivateKey;
+  protected masterPublicKey!: PublicKey;
+  protected privateKey?: PrivateKey;
+  protected publicKey!: PublicKey;
+  protected publicKeyType: string;
+  protected wifType: string;
+  protected wifPrefix?: number;
+  protected derivation: ElectrumDerivation;
+
+  constructor(options: HDOptionsInterface = {
+    publicKeyType: PUBLIC_KEY_TYPES.UNCOMPRESSED
+  }) {
+    super(options);
+
+    this.publicKeyType = options.publicKeyType ?? PUBLIC_KEY_TYPES.UNCOMPRESSED;
+    if (this.publicKeyType === PUBLIC_KEY_TYPES.UNCOMPRESSED) {
+      this.wifType = WIF_TYPES.WIF;
+    } else if (this.publicKeyType === PUBLIC_KEY_TYPES.COMPRESSED) {
+      this.wifType = WIF_TYPES.WIF_COMPRESSED;
+    } else {
+      throw new BaseError('Invalid public key type', {
+        expected: PUBLIC_KEY_TYPES.getTypes(), got: this.publicKeyType
+      });
+    }
+
+    this.wifPrefix = options.wifPrefix ?? Bitcoin.NETWORKS.MAINNET.WIF_PREFIX;
+    this.derivation = new ElectrumDerivation({
+      change: options.change, address: options.address
+    });
+  }
+
+  static getName(): string {
+    return 'Electrum-V1';
+  }
+
+  fromSeed(seed: Uint8Array | string | Seed): this {
+    try {
+      this.seed = getBytes(seed instanceof Seed ? seed.getSeed() : seed);
+      return this.fromPrivateKey(this.seed);
+    } catch {
+      throw new SeedError('Invalid seed data');
+    }
+  }
+
+  fromPrivateKey(key: Uint8Array | string): this {
+    try {
+      this.masterPrivateKey = SLIP10Secp256k1PrivateKey.fromBytes(getBytes(key));
+      this.masterPublicKey = this.masterPrivateKey.getPublicKey();
+      this.fromDerivation(this.derivation);
+      return this;
+    } catch {
+      throw new PrivateKeyError('Invalid private key data');
+    }
+  }
+
+  fromWIF(wif: string): this {
+    if (this.wifPrefix == null) throw new WIFError('WIF prefix is required');
+    return this.fromPrivateKey(wifToPrivateKey(wif, this.wifPrefix));
+  }
+
+  fromPublicKey(key: Uint8Array | string): this {
+    try {
+      this.masterPublicKey = SLIP10Secp256k1PublicKey.fromBytes(getBytes(key));
+      this.fromDerivation(this.derivation);
+      return this;
+    } catch {
+      throw new PublicKeyError('Invalid public key error');
+    }
+  }
+
+  fromDerivation(derivation: ElectrumDerivation): this {
+    if (!checkTypeMatch(derivation, ElectrumDerivation).isValid) {
+      throw new DerivationError('Invalid derivation instance', {
+        expected: ElectrumDerivation.name, got: derivation.constructor.name
+      });
+    }
+
+    this.derivation = derivation;
+    this.drive(derivation.getChange(), derivation.getAddress());
+    return this;
+  }
+
+  updateDerivation(derivation: ElectrumDerivation): this {
+    return this.fromDerivation(derivation);
+  }
+
+  cleanDerivation(): this {
+    this.derivation.clean();
+    this.fromDerivation(this.derivation);
+    return this;
+  }
+
+  drive(changeIndex: number, addressIndex: number): this {
+    const sequence = doubleSha256(concatBytes(
+      Buffer.from(`${addressIndex}:${changeIndex}:`, 'utf-8'),
+      this.masterPublicKey.getRawUncompressed().slice(1)
+    ));
+
+    if (this.masterPrivateKey) {
+      const privateKeyInt =
+        (bytesToInteger(this.masterPrivateKey.getRaw()) + bytesToInteger(sequence)) % SLIP10Secp256k1ECC.ORDER;
+      this.privateKey = SLIP10Secp256k1PrivateKey.fromBytes(
+        integerToBytes(privateKeyInt, SLIP10Secp256k1PrivateKey.getLength())
+      );
+      this.publicKey = this.privateKey.getPublicKey();
+    } else {
+      this.publicKey = SLIP10Secp256k1PublicKey.fromPoint(
+        this.masterPublicKey.getPoint().add(
+          SLIP10Secp256k1ECC.GENERATOR.multiply(bytesToInteger(sequence))
+        )
+      );
+    }
+    return this;
+  }
+
+  getSeed(): string | undefined {
+    return this.seed ? bytesToString(this.seed) : undefined;
+  }
+
+  getMasterPrivateKey(): string | undefined {
+    return this.masterPrivateKey ? bytesToString(this.masterPrivateKey.getRaw()) : undefined;
+  }
+
+  getMasterWIF(wifType?: string): string | undefined {
+    if (!this.masterPrivateKey || this.wifPrefix == null) return undefined;
+
+    const type = wifType ?? this.wifType;
+    return privateKeyToWIF(this.getMasterPrivateKey()!, type, this.wifPrefix);
+  }
+
+  getMasterPublicKey(publicKeyType: string = this.publicKeyType): string {
+    if (publicKeyType === PUBLIC_KEY_TYPES.UNCOMPRESSED) {
+      return bytesToString(this.masterPublicKey.getRawUncompressed());
+    } else if (publicKeyType === PUBLIC_KEY_TYPES.COMPRESSED) {
+      return bytesToString(this.masterPublicKey.getRawCompressed());
+    }
+    throw new BaseError(`Invalid ${this.getName()} public key type`, {
+      expected: Object.values(PUBLIC_KEY_TYPES), got: publicKeyType
+    });
+  }
+
+  getPrivateKey(): string | undefined {
+    return this.privateKey ? bytesToString(this.privateKey.getRaw()) : undefined;
+  }
+
+  getWIF(wifType?: string): string | undefined {
+    if (!this.privateKey || this.wifPrefix == null) return undefined;
+
+    const type = wifType ?? this.wifType;
+    return privateKeyToWIF(this.getPrivateKey()!, type, this.wifPrefix);
+  }
+
+  getWIFType(): string {
+    return this.wifType;
+  }
+
+  getPublicKey(publicKeyType: string = this.publicKeyType): string {
+    if (publicKeyType === PUBLIC_KEY_TYPES.UNCOMPRESSED) {
+      return bytesToString(this.publicKey.getRawUncompressed());
+    } else if (publicKeyType === PUBLIC_KEY_TYPES.COMPRESSED) {
+      return bytesToString(this.publicKey.getRawCompressed());
+    }
+    throw new BaseError(`Invalid ${this.getName()} public key type`, {
+      expected: Object.values(PUBLIC_KEY_TYPES), got: publicKeyType
+    });
+  }
+
+  getPublicKeyType(): string {
+    return this.publicKeyType;
+  }
+
+  getCompressed(): string {
+    return bytesToString(this.publicKey.getRawCompressed());
+  }
+
+  getUncompressed(): string {
+    return bytesToString(this.publicKey.getRawUncompressed());
+  }
+
+  getAddress(options: HDAddressOptionsInterface = {
+    publicKeyAddressPrefix: Bitcoin.NETWORKS.MAINNET.PUBLIC_KEY_ADDRESS_PREFIX
+  }): string {
+    return P2PKHAddress.encode(this.publicKey!, {
+      publicKeyAddressPrefix: options.publicKeyAddressPrefix ?? Bitcoin.NETWORKS.MAINNET.PUBLIC_KEY_ADDRESS_PREFIX,
+      publicKeyType: this.publicKeyType
+    });
+  }
+}
---
+++ src/hds/electrum/v2.ts
@@ -0,0 +1,175 @@
+// SPDX-License-Identifier: MIT
+
+import { HD } from '../hd';
+import { ElectrumDerivation, CustomDerivation } from '../../derivations';
+import { PUBLIC_KEY_TYPES, MODES, WIF_TYPES } from '../../const';
+import { P2PKHAddress, P2WPKHAddress } from '../../addresses';
+import { privateKeyToWIF } from '../../wif';
+import { Bitcoin } from '../../cryptocurrencies';
+import { BIP32HD } from '../bip32';
+import { checkTypeMatch } from '../../utils';
+import { Seed } from '../../seeds';
+import { BaseError, AddressError, DerivationError } from '../../exceptions';
+import { HDAddressOptionsInterface, HDOptionsInterface } from '../../interfaces';
+
+export class ElectrumV2HD extends HD {
+
+  protected mode: string;
+  protected wifType: string;
+  protected publicKeyType: string;
+  protected wifPrefix?: number;
+  protected derivation: ElectrumDerivation;
+  protected bip32HD: BIP32HD;
+
+  constructor(options: HDOptionsInterface = {
+    publicKeyType: PUBLIC_KEY_TYPES.UNCOMPRESSED,
+    mode: MODES.STANDARD
+  }) {
+    super(options);
+
+    this.mode = options.mode ?? MODES.STANDARD;
+    if (!MODES.getTypes().includes(this.mode)) {
+      throw new BaseError(`Invalid ${this.getName()} mode`, {
+        expected: MODES.getTypes(),
+        got: this.mode
+      });
+    }
+
+    this.publicKeyType = options.publicKeyType ?? PUBLIC_KEY_TYPES.UNCOMPRESSED;
+    if (this.publicKeyType === PUBLIC_KEY_TYPES.UNCOMPRESSED) {
+      this.wifType = WIF_TYPES.WIF;
+    } else if (this.publicKeyType === PUBLIC_KEY_TYPES.COMPRESSED) {
+      this.wifType = WIF_TYPES.WIF_COMPRESSED;
+    } else {
+      throw new BaseError('Invalid public key type', {
+        expected: PUBLIC_KEY_TYPES.getTypes(), got: this.publicKeyType
+      });
+    }
+
+    this.wifPrefix = options.wifPrefix ?? Bitcoin.NETWORKS.MAINNET.WIF_PREFIX;
+    this.derivation = new ElectrumDerivation({
+      change: options.change, address: options.address
+    });
+    this.bip32HD = new BIP32HD(Bitcoin.ECC, {
+      publicKeyType: this.publicKeyType
+    });
+  }
+
+  getName(): string {
+    return 'Electrum-V2';
+  }
+
+  fromSeed(seed: Uint8Array | string | Seed): this {
+    this.bip32HD.fromSeed(seed);
+    this.fromDerivation(this.derivation);
+    return this;
+  }
+
+  fromDerivation(derivation: ElectrumDerivation): this {
+    if (!checkTypeMatch(derivation, ElectrumDerivation).isValid) {
+      throw new DerivationError('Invalid derivation instance', {
+        expected: ElectrumDerivation.name, got: derivation.constructor.name
+      });
+    }
+
+    this.derivation = derivation;
+    this.drive(derivation.getChange(), derivation.getAddress());
+    return this;
+  }
+
+  updateDerivation(derivation: ElectrumDerivation): this {
+    return this.fromDerivation(derivation);
+  }
+
+  cleanDerivation(): this {
+    this.derivation.clean();
+    this.fromDerivation(this.derivation);
+    return this;
+  }
+
+  drive(changeIndex: number, addressIndex: number): this {
+    const custom = new CustomDerivation();
+    if (this.mode === MODES.SEGWIT) {
+      custom.fromIndex(0, true); // Hardened
+    }
+    custom.fromIndex(changeIndex);
+    custom.fromIndex(addressIndex);
+    this.bip32HD.updateDerivation(custom);
+    return this;
+  }
+
+  getMode(): string {
+    return this.mode;
+  }
+
+  getSeed(): string | undefined {
+    return this.bip32HD.getSeed();
+  }
+
+  getMasterPrivateKey(): string | undefined {
+    return this.bip32HD.getRootPrivateKey();
+  }
+
+  getMasterWIF(wifType?: string): string | undefined {
+    if (this.wifPrefix == null) return undefined;
+    const type = wifType ?? this.wifType;
+    return privateKeyToWIF(this.getMasterPrivateKey()!, type, this.wifPrefix);
+  }
+
+  getMasterPublicKey(publicKeyType?: string): string {
+    return this.bip32HD.getRootPublicKey(publicKeyType ?? this.publicKeyType)!;
+  }
+
+  getPrivateKey(): string | undefined {
+    return this.bip32HD.getPrivateKey();
+  }
+
+  getWIF(wifType?: string): string | undefined {
+    if (this.wifPrefix == null) return undefined;
+    const type = wifType ?? this.wifType;
+    return privateKeyToWIF(this.getPrivateKey()!, type, this.wifPrefix);
+  }
+
+  getWIFType(): string {
+    return this.wifType;
+  }
+
+  getPublicKey(publicKeyType?: string): string {
+    return this.bip32HD.getPublicKey(publicKeyType ?? this.publicKeyType);
+  }
+
+  getPublicKeyType(): string {
+    return this.publicKeyType;
+  }
+
+  getUncompressed(): string {
+    return this.bip32HD.getUncompressed();
+  }
+
+  getCompressed(): string {
+    return this.bip32HD.getCompressed();
+  }
+
+  getAddress(options: HDAddressOptionsInterface = {
+    publicKeyAddressPrefix: Bitcoin.NETWORKS.MAINNET.PUBLIC_KEY_ADDRESS_PREFIX,
+    hrp: Bitcoin.NETWORKS.MAINNET.HRP,
+    witnessVersion: Bitcoin.NETWORKS.MAINNET.WITNESS_VERSIONS.P2WPKH
+  }): string {
+
+    if (this.mode === MODES.STANDARD) {
+      return P2PKHAddress.encode(this.getPublicKey(), {
+        publicKeyAddressPrefix: options.publicKeyAddressPrefix ?? Bitcoin.NETWORKS.MAINNET.PUBLIC_KEY_ADDRESS_PREFIX,
+        publicKeyType: this.publicKeyType
+      });
+    } else if (this.mode === MODES.SEGWIT) {
+      return P2WPKHAddress.encode(this.getPublicKey(), {
+        hrp: options.hrp ?? Bitcoin.NETWORKS.MAINNET.HRP,
+        witnessVersion: options.witnessVersion ?? Bitcoin.NETWORKS.MAINNET.WITNESS_VERSIONS.P2WPKH,
+        publicKeyType: this.publicKeyType
+      });
+    }
+    throw new AddressError(`Invalid ${this.getName()} mode`, {
+      expected: MODES.getTypes(), got: this.mode
+    });
+  }
+}
