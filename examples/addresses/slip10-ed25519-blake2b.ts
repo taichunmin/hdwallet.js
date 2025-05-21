1948ccddf914ec6cdd8a7de8d90ebb3e43b662b7
meherett <meherett.batu@gmail.com>
2025-05-21 12:31:02 +0300

    Add: SLIP10-Ed25519-Blake2b addresses

---
+++ examples/addresses/slip10-ed25519-blake2b.ts
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+import {
+  PrivateKey, PublicKey, SLIP10Ed25519Blake2bPrivateKey
+} from '../../src/ecc';
+import { NanoAddress } from '../../src/addresses';
+import { bytesToString, getBytes } from '../../src/utils';
+
+const privateKey: PrivateKey = SLIP10Ed25519Blake2bPrivateKey.fromBytes(getBytes(
+  'bb37794073e5094ebbfcfa070e9254fe6094b56e7cccb094a2304c5eccccdc07'
+));
+const publicKey: PublicKey = privateKey.getPublicKey();
+
+console.log('Private Key:', bytesToString(privateKey.getRaw()));
+console.log('Uncompressed Public Key:', bytesToString(publicKey.getRawUncompressed()));
+console.log('Compressed Public Key:', bytesToString(publicKey.getRawCompressed()), '\n');
+
+const nanoAddress = NanoAddress.encode(publicKey);
+const nanoAddressHash = NanoAddress.decode(nanoAddress);
+console.log('Nano Address:', nanoAddress, nanoAddressHash);
