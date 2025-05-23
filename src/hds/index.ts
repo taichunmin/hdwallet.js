f5616157608e65428095f8ea07b1549bdf32b76a
meherett <meherett.batu@gmail.com>
2025-05-23 08:57:31 +0300

    Add: index.ts for HDs

---
+++ src/hds/index.ts
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+import { HD } from './hd'
+import { BaseError } from '../exceptions';
+import { BIP32HD } from './bip32';
+import { BIP44HD } from './bip44';
+import { BIP49HD } from './bip49';
+import { BIP84HD } from './bip84';
+import { BIP141HD } from './bip141';
+import { BIP86HD } from './bip86';
+import { CardanoHD } from './cardano';
+import { ElectrumV1HD } from './electrum/v1';
+import { ElectrumV2HD } from './electrum/v2';
+import { MoneroHD } from './monero';
+
+export class HDS {
+
+  private static dictionary: Record<string, typeof HD> = {
+    'BIP32': BIP32HD,
+    'BIP44': BIP44HD,
+    'BIP49': BIP49HD,
+    'BIP84': BIP84HD,
+    'BIP86': BIP86HD,
+    'BIP141': BIP141HD,
+    'Cardano': CardanoHD,
+    'Electrum-V1': ElectrumV1HD,
+    'Electrum-V2': ElectrumV2HD,
+    'Monero': MoneroHD
+  }
+
+  static getNames(): string[] {
+    return Object.keys(this.dictionary);
+  }
+
+  static getClasses(): typeof HD[] {
+    return Object.values(this.dictionary);
+  }
+
+  static getHDClass(name: string): typeof HD | any {
+    if (!this.isHD(name)) {
+      throw new BaseError(
+        'Invalid HD name', { expected: this.getNames(), got: name }
+      )
+    }
+    return this.dictionary[name];
+  }
+
+  static isHD(name: string): boolean {
+    return this.getNames().includes(name);
+  }
+}
+
+export {
+  HD,
+  BIP32HD,
+  BIP44HD,
+  BIP49HD,
+  BIP84HD,
+  BIP86HD,
+  BIP141HD,
+  CardanoHD,
+  ElectrumV1HD,
+  ElectrumV2HD,
+  MoneroHD
+}
